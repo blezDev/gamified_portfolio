e28555e81d6277aad1395329966824a6641017ab
blezDev <msaikrishnapatnaik@gmail.com>
2024-06-18 17:26:41 +0530

    First commit

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in Android-Kotlin/JavaScript these days.
+    I've made a couple of apps in these languages. I also like Java and Springboot. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/blezDev" target="_blank">Github</a>!`,
+    "cs-degree": `This is my CS degree (2020-2024 Batch). I hung it on the wall because I'm proud of it. It gave me a good foundation.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube and play games.`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@StevdzaSan" target="_blank">Stevdza-San</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@PhilippLackner" target="_blank">
+Philipp Lackner</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/blezDev/resume/blob/main/Resume.pdf" target="_blank">Check it out?</a>
+  Contact me at <a href="https://mail.google.com/mail/?view=cm&fs=1&to=msaikrishnapatnaik@gmail.com" target="_blank">msaikrishnapatnaik@gmail</a> if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of JSLegendDev tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+    library: `There are a lot of programming books, novels and self help books on my shelves. There is even one discussing about taste in samber of one's house help in judge the family love.
+  I probably only read one of them. Who else compulsively buys books without ever finishing them?`,
+    exit: `If you want to exit BlezDev's portfolio, just close the tab.`,
+};
